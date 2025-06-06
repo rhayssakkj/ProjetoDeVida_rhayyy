6c068f515ade8381c6480767b786915cee09c7a9
rhayssakkj <132940117+rhayssakkj@users.noreply.github.com>
2025-06-06 11:15:11 -0300

    Add files via upload

--- js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
+++ js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
@@ -1,12 +1,26 @@
-const botoes = document.querySelectorAll(".botao"); 
+const botoes = document.querySelectorAll (".botao");
+const textos = document.querySelectorAll (".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
+for(let i=0;i < botoes.length; i++){
     botoes[i].onclick = function(){
 
-        for(let j=0;j < botoes.length;j++){
+        for(let j=0;j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo");
+      botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
     }
     console.log();
 }
+
+const contadores = document.querySelectorAll (".contador");
+let tempoAtual = new Date ();
+const tempoObjetivo1 = new Date("2025-12-12T00:00:00") - tempoAtual;
+contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo2 = new Date("2035-10-23T00:00:00") - tempoAtual;
+contadores[1].textContent = tempoObjetivo2;
+const tempoObjetivo3 = new Date("2029-02-10T00:00:00") -tempoAtual;
+contadores[2].textContent = tempoObjetivo3;
+const tempoObjetivo4 = new Date("2026-01-01T00:00:00") - tempoAtual;
+contadores[3].textContent = tempoObjetivo4;
